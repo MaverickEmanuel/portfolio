1300ab85b0a2e54196eee4f96ee01498c0bc718e
maverickemanuel <mavjamesemanuel@gmail.com>
2023-05-30 12:14:00 -0400

    Updates

---
+++ precache-manifest.a7a0f92dc422bb193d2f8165269903f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3e72e02a82b4433840d",
+    "url": "/portfolio/static/css/main.96bd56ff.chunk.css"
+  },
+  {
+    "revision": "a3e72e02a82b4433840d",
+    "url": "/portfolio/static/js/main.a3e72e02.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/portfolio/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/portfolio/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "f09c399266580b3532b9",
+    "url": "/portfolio/static/js/runtime~main.f09c3992.js"
+  },
+  {
+    "revision": "a1f94279d9310a1233c2c7ba178c6418",
+    "url": "/portfolio/index.html"
+  }
+];
