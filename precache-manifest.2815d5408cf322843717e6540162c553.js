5d822efe718f03e62efc3333f34d73ba545ad7c3
maverickemanuel <mavjamesemanuel@gmail.com>
2023-05-30 20:05:51 -0400

    Updates

---
+++ precache-manifest.2815d5408cf322843717e6540162c553.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a5980a3587fe5220bf0",
+    "url": "/portfolio/static/css/main.4cdd97c8.chunk.css"
+  },
+  {
+    "revision": "3a5980a3587fe5220bf0",
+    "url": "/portfolio/static/js/main.3a5980a3.chunk.js"
+  },
+  {
+    "revision": "fa677b773aaf13c72ca8",
+    "url": "/portfolio/static/js/1.fa677b77.chunk.js"
+  },
+  {
+    "revision": "5009651c512714aec54f",
+    "url": "/portfolio/static/js/2.5009651c.chunk.js"
+  },
+  {
+    "revision": "93b4b4731227c9718938",
+    "url": "/portfolio/static/js/runtime~main.93b4b473.js"
+  },
+  {
+    "revision": "c8828b255fbaead179359660c8d89c35",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9aa2bf39586c020abd984174be769483.js"
+  "/portfolio/precache-manifest.2815d5408cf322843717e6540162c553.js"
 );
 
 workbox.clientsClaim();
