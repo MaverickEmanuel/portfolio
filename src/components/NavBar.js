aeaaf124ba45ebf546cfdf6c18daccd523b7ecc2
maverickemanuel <mavjamesemanuel@gmail.com>
2023-05-30 20:12:58 -0400

    Fixed routing for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import '../styles/NavBar.css';
 
 function NavBar() {
     return (
         <div className="NavBar">
             <div className="Headers">
-                <a href="/projects">Projects</a>
-                <a href="/portfolio">Home</a>
-                <a href="/experience">Experience</a>
+                <Link to="/projects">Projects</Link>
+                <Link to="/portfolio">Home</Link>
+                <Link to="/experience">Experience</Link>
             </div>
         </div>
     )
